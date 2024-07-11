b115dfe9d1e3da33db7a90cf832e282b1f58874a
maldron0309 <bagjinhyeong640@gmail.com>
2024-07-11 10:23:08 +0900

    Input

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,4 +1,13 @@
 extends Node
 
 func _ready():
+	$Label.text = "Hello World"
+	$Label.modulate = Color.GREEN
 	
+	
+func _input(event):
+	if event.is_action_pressed("action"):
+		$Label.modulate = Color.RED
+		
+	if event.is_action_released("action"):
+		$Label.modulate = Color.GREEN
